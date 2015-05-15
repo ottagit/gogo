65b3dce807a5505593e83233b17fdbc85610039e
ottagit <ottachris13@yahoo.com>
2015-05-15 15:14:10 +0300

    Yet another commit

--- Demo.java
+++ Demo.java
@@ -1,7 +1,23 @@
+import java.io.*;
+import java.util.Scanner;
+
+class Notmain
+{
+ public void calledinMain(String commit)
+ {
+  Scanner scan = new Scanner(System.in);
+  System.out.println("Enter commit message------>> ");
+  String msg = scan.nextLine();
+  msg = commit;
+ }
+}
+
 public class Modified
 {
  public static void main(String args[])
  {
-  System.out.println("Modified commit.");
+  Notmain not = new Notmain();
+  System.out.println("Calling class....\n");
+  not.calleinMain();
  }
 }
